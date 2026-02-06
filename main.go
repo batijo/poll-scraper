01e9794d4705e2f55722968e21607ea424e36597
Titas Jonas B <titasbajorunas@gmail.com>
2026-02-06 05:44:12 +0200

    refactor(01-01): refactor main.go to launch Wails application
    - Replace HTTP server startup with Wails desktop launcher
    - Add embedded filesystem for frontend assets
    - Move config/logger/server initialization to App.Startup
    - Create placeholder frontend/build directory for embed directive
    - Remove old initLogger function and errorLog global
    
    Task 3 of 3: Wails Foundation Phase 1

--- main.go
+++ main.go
@@ -1,57 +1,35 @@
 package main
 
 import (
-	"fmt"
-	"log/slog"
-	"os"
+	"embed"
 
-	"github.com/batijo/poll-scraper/config"
-	"github.com/batijo/poll-scraper/server"
-	"github.com/batijo/poll-scraper/utils"
-	"github.com/batijo/poll-scraper/utils/file"
+	"github.com/wailsapp/wails/v2"
+	"github.com/wailsapp/wails/v2/pkg/options"
+	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
 )
 
-var errorLog *slog.Logger
-
-func initLogger(debug bool) {
-	logFile, err := os.OpenFile("error.log", os.O_CREATE|os.O_APPEND|os.O_WRONLY, utils.FileMode)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "failed to open log file: %v\n", err)
-		os.Exit(1)
-	}
-	errorLog = slog.New(slog.NewTextHandler(logFile, &slog.HandlerOptions{
-		Level:     slog.LevelError,
-		AddSource: true,
-	}))
-
-	if debug {
-		slog.SetDefault(slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
-			Level: slog.LevelDebug,
-		})))
-	}
-}
+//go:embed all:frontend/build
+var assets embed.FS
 
 func main() {
-	cfg, err := config.Load("config.json")
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "failed to load config: %v\n", err)
-		os.Exit(1)
-	}
-	initLogger(cfg.Debug)
+	app := NewApp()
 
-	srv := server.New(cfg)
-	if err := file.InitFiles(cfg); err != nil {
-		errorLog.Error("failed to init files", "err", err)
-		os.Exit(1)
-	}
-	if err := file.StartWriting(cfg); err != nil {
-		errorLog.Error("failed to start writer", "err", err)
-		os.Exit(1)
-	}
-	srv.Addr = fmt.Sprintf("%s:%d", cfg.IP, cfg.Port)
-	slog.Info("server starting", "address", srv.Addr)
-	if err := srv.ListenAndServe(); err != nil {
-		errorLog.Error("server stopped with error", "err", err)
-		os.Exit(1)
+	err := wails.Run(&options.App{
+		Title:  "Poll Scraper",
+		Width:  1024,
+		Height: 768,
+		AssetServer: &assetserver.Options{
+			Assets: assets,
+		},
+		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
+		OnStartup:        app.Startup,
+		OnShutdown:       app.Shutdown,
+		Bind: []interface{}{
+			app,
+		},
+	})
+
+	if err != nil {
+		println("Error:", err.Error())
 	}
 }
